b96b1d814333adffbe69aebde1a56fde3f23ed86
Zhiwei.Zhang <zz2810@nyu.edu>
2024-12-01 12:07:38 -0500

    from zz

--- bdad/src/main/scala/consumer/KafkaConsumer.scala
+++ bdad/src/main/scala/consumer/KafkaConsumer.scala
@@ -2,7 +2,7 @@ package consumer
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, from_json}
-import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType, TimestampType}
+import org.apache.spark.sql.types._
 
 object KafkaConsumer {
   def main(args: Array[String]): Unit = {
@@ -10,13 +10,6 @@ object KafkaConsumer {
       .appName("Transaction Consumer")
       .master("local[*]")
       .getOrCreate();
-
-    val df = spark
-      .readStream
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "nyu-dataproc-w-0:9092")
-      .option("subscribe", "bdad-g3")
-      .load()
 
     val schema = StructType(Array(
       StructField("userid", StringType, true),
@@ -29,11 +22,20 @@ object KafkaConsumer {
       StructField("country", StringType, true)
     ));
 
+    val df = spark
+      .readStream
+      .schema(schema)
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "nyu-dataproc-w-0:9092")
+      .option("subscribe", "bdad-g3")
+      .load()
+
     val jsonDf = df.selectExpr("CAST(value AS STRING) AS value")
       .select(from_json(col("value"), schema).alias("data"))
       .select("data.*")
 
-    val query = jsonDf.writeStream
+    val query = jsonDf
+      .writeStream
       .outputMode("append")
       .format("console")
       .option("truncate", "false")
