fc2dde9f6066db2917b05c44288a1636ef6d7f37
bblove5210 <55412824+bblove5210@users.noreply.github.com>
2024-12-08 20:21:14 -0500

    updated producer

--- bdad/producer/src/main/scala/KafkaProducer.scala
+++ bdad/producer/src/main/scala/KafkaProducer.scala
@@ -5,52 +5,57 @@ import org.apache.spark.sql.types._;
 import org.apache.spark.sql.functions._;
 
 object KafkaProducer {
+  val Date = "2024-9-03";
+
+  def splitDropoffPickup(df: org.apache.spark.sql.DataFrame): org.apache.spark.sql.DataFrame = {
+    val pickupDF = df.select(
+      lit("pickup").alias("event_type"),
+      col("pickup_datetime").alias("event_time"),
+      col("PULocationID").alias("location_id")
+    ).filter(to_date(col("pickup_datetime")) === Date);
+
+    val dropoffDF = df.select(
+      lit("dropoff").alias("event_type"),
+      col("dropoff_datetime").alias("event_time"),
+      col("DOLocationID").alias("location_id")
+    ).filter(to_date(col("dropoff_datetime")) === Date);
+
+    return pickupDF.union(dropoffDF);
+  }
+
+  def writeToKafka(df: org.apache.spark.sql.DataFrame): Unit = {
+    df.selectExpr("CAST(NULL AS STRING) AS key", "to_json(struct(*)) AS value")
+      .write
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "nyu-dataproc-w-0:9092")
+      .option("topic", "bdad-g3")
+      .save;
+  }
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .appName("Transaction Producer")
       .master("local[*]")
       .getOrCreate();
 
-    val schema = StructType(Array(
-      StructField("userid", StringType, true),
-      StructField("transactionid", StringType, true),
-      StructField("transactionTime", TimestampType, true),
-      StructField("itemcode", StringType, true),
-      StructField("itemdescription", StringType, true),
-      StructField("numberofitempurchased", IntegerType, true),
-      StructField("costperitem", DoubleType, true),
-      StructField("country", StringType, true)
-    ));
-
     val homeDir = System.getProperty("user.home")
-    val transactionDF = spark.read
-      .schema(schema)
-      .csv(s"$homeDir/transaction_data.csv")
+    val DF = spark.read.parquet(s"$homeDir/fhvhv_tripdata_2024-09.parquet");
+    val dayDF = splitDropoffPickup(DF.filter(to_date(col("dropoff_datetime")) === Date || to_date(col("pickup_datetime")) === Date))
+      .sort(asc("event_time"))
+      .cache;
 
-    val kvDF = transactionDF
-      .withColumn("key", col("userid"))
-      .withColumn("value", to_json(struct(
-        col("transactionid"),
-        col("transactionTime"),
-        col("itemcode"),
-        col("itemdescription"),
-        col("numberofitempurchased"),
-        col("costperitem"),
-        col("country")
-      ))).selectExpr("key", "value");
+    writeToKafka(dayDF.filter(hour(col("dropoff_datetime")) < 20));
 
-    val splitWeight = Array.fill(100)(1.0);
-
-    val splitDFs = kvDF.randomSplit(splitWeight, 42);
-    
-    for (splitDF <- splitDFs) {
-      splitDF.write
-      .format("kafka")
-      .option("kafka.bootstrap.servers", "nyu-dataproc-w-0:9092")
-      .option("topic", "bdad-g3")
-      .save;
-
-      Thread.sleep(1000);
+    for (ihour <- 20 to 23) {
+      val hourDF = dayDF.filter(hour(col("event_time")) === ihour).cache;
+      for (iminute <- 0 to 59) {
+        val minuteDF = hourDF.filter(minute(col("event_time")) === iminute).cache
+        for (isecond <- 0 to 59) {
+          val secondDF = minuteDF.filter(second(col("event_time")) === isecond).cache;
+          writeToKafka(secondDF);
+          Thread.sleep(1000);
+        }
+      }
     }
 
     spark.stop();
