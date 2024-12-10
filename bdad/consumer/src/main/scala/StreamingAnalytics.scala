84cde1d0dfd6f580a9fa2c2807c5ea70cea55e37
bblove5210 <55412824+bblove5210@users.noreply.github.com>
2024-12-09 22:48:17 -0500

    adds windowedSum to calculate change in ongoing trips

--- bdad/consumer/src/main/scala/StreamingAnalytics.scala
+++ bdad/consumer/src/main/scala/StreamingAnalytics.scala
@@ -10,6 +10,16 @@ object StreamingAnalytics {
         val locationDf = spark.read.option("header", "true").csv(s"$homeDir/taxi_zone_lookup.csv")
             .select(col("LocationID").alias("location_id"), concat_ws(",", col("Zone"), col("Borough")).alias("Location")).cache;
 
+        // the number of pickups in the last five minutes
+        val windowedSums = jsonDf.withColumn("event_time", col("event_time").cast("timestamp"))
+            .withWatermark("event_time", "5 minute")
+            .groupBy(window(col("event_time"), "5 minute", "1 minute"))
+            .agg(
+                sum(when(col("event_type") === "pickup", 1).otherwise(0)).alias("num_pickups"),
+                sum(when(col("event_type") === "dropoff", 1).otherwise(0)).alias("num_dropoffs")
+            )
+            .withColumn("change_in_num_ongoing_trips", col("num_pickups") - col("num_dropoffs"));
+
         // zone with the most pickups in the last five minutes
         val windowedCounts = df.withColumn("event_time", col("event_time").cast("timestamp"))
             .filter(col("event_type") === "pickup")
@@ -18,12 +28,18 @@ object StreamingAnalytics {
             .agg(count("*").alias("num_pickups"))
             .orderBy(col("num_pickups").desc)
             .join(locationDf, "location_id")
-            .limit(1)
+            .limit(1);
 
-        windowedCounts.writeStream
+        val sumQuery = windowedSums.writeStream
             .format("console")
             .outputMode("append")
             .option("truncate", "false")
-            .start()
+            .start();
+
+        val countQuery = windowedCounts.writeStream
+            .format("console")
+            .outputMode("append")
+            .option("truncate", "false")
+            .start();
     }
 }
